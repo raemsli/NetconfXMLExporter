737e9f69ce431827da6120370388ec6792eda14e
raemsli <ramon.stutz@ost.ch>
2024-12-15 11:14:33 +0100

    Update testcode_toxml.py

--- Demo/testcode_toxml.py
+++ Demo/testcode_toxml.py
@@ -193,4 +193,7 @@ def createModel():
     return parseString(xml_string).toprettyxml()
 
 if __name__ == "__main__":
-    createModel()
+    with open(XML_Files/demo.xml", "w") as file:
+        file.write("createModel()")
+
+    
